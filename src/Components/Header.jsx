e801b960c7e10e0c87d391100e9cd883ba1adfcc
mdhossain07 <mrabir8097@gmail.com>
2023-10-17 19:50:54 +0600

    project setup done & navbar complete

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import Banner from "./Banner";
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar />
+      <Banner />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Routes/Route.jsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../Layout/Layout";
+import Home from "../Pages/Home";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
+import Cart from "../Pages/Cart";
+import AddProducts from "../Pages/AddProducts";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/add-products",
+        element: <AddProducts />,
+      },
+    ],
+  },
+]);
+
+export default router;

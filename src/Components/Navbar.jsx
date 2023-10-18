b038348794d4bb681e3cff8bf12daca842b5d8b3
mdhossain07 <mrabir8097@gmail.com>
2023-10-18 07:02:50 +0600

    firebase authentication done with Google

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -58,7 +58,7 @@ const Navbar = () => {
               {navLinks}
             </ul>
           </div>
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+          <a className="btn btn-ghost normal-case text-xl">Automotive Shop</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">{navLinks}</ul>
